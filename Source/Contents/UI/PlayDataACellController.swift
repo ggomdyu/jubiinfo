7288182794ad93567da6a493e87b221bd9e4824a
ggomdyu <ggomdyu@gmail.com>
2018-12-09 22:51:19 +0900

    유빌리티에 따라서 유빌리티 텍스트 색상 다르게 하기

--- Source/Contents/UI/PlayDataACellController.swift
+++ Source/Contents/UI/PlayDataACellController.swift
@@ -27,6 +27,8 @@ class PlayDataACellController : ViewController {
         }
         
         jubilityLabel.text = "\(myPlayDataPageCache.jubility)"
+        jubilityLabel.textColor = self.getJubilityColor(jubility: myPlayDataPageCache.jubility)
+        
         totalScoreLabel.text = self.createNumberWithComma(number: myPlayDataPageCache.totalScore)
         lastPlayedTimeLabel.text = myPlayDataPageCache.lastPlayedTime
         lastPlayedLocationLabel.text = myPlayDataPageCache.lastPlayedLocation
@@ -35,6 +37,48 @@ class PlayDataACellController : ViewController {
 }
 
 extension PlayDataACellController {
+    
+    private func getJubilityColor(jubility: Float) -> UIColor {
+        
+        // GOLD
+        if jubility >= 9500 {
+            return UIColor(red: 250 / 255, green: 215 / 255, blue: 73 / 255, alpha: 1)
+        }
+        // ORANGE
+        else if jubility >= 8500 {
+            return UIColor(red: 222 / 255, green: 162 / 255, blue: 91 / 255, alpha: 1)
+        }
+        // PINK
+        else if jubility >= 7000 {
+            return UIColor(red: 212 / 255, green: 168 / 255, blue: 238 / 255, alpha: 1)
+        }
+        // PURPLE
+        else if jubility >= 5500 {
+            return UIColor(red: 132 / 255, green: 131 / 255, blue: 240 / 255, alpha: 1)
+        }
+        // VIOLET
+        else if jubility >= 4000 {
+            return UIColor(red: 143 / 255, green: 132 / 255, blue: 207 / 255, alpha: 1)
+        }
+        // BLUE
+        else if jubility >= 2500 {
+            return UIColor(red: 101 / 255, green: 130 / 255, blue: 195 / 255, alpha: 1)
+        }
+        // LIGHT BLUE
+        else if jubility >= 1500 {
+            return UIColor(red: 171 / 255, green: 246 / 255, blue: 251 / 255, alpha: 1)
+        }
+        // GREEN
+        else if jubility >= 750 {
+            return UIColor(red: 111 / 255, green: 171 / 255, blue: 94 / 255, alpha: 1)
+        }
+        // YELLOW GREEN
+        else if jubility >= 750 {
+            return UIColor(red: 185 / 255, green: 204 / 255, blue: 74 / 255, alpha: 1)
+        }
+        
+        return UIColor(red: 153 / 255, green: 153 / 255, blue: 153 / 255, alpha: 1)
+    }
     
     private func createNumberWithComma(number: Int64) -> String {
         var needCommaCount: Int = 0;
