36619c4b5a1893a76a5618d9019db3efe3977593
jhcha <jhcha@bluehole.net>
2019-07-18 13:38:51 +0900

    파싱 코드만 좀 추가

--- CustomMusicDataAutoGen/CustomMusicDataAutoGen.py
+++ CustomMusicDataAutoGen/CustomMusicDataAutoGen.py
@@ -4,8 +4,18 @@ from bs4 import BeautifulSoup
 customMusicDataJson: str = "{\"music\":{"
 
 def processMusicListCell(parsedHtml):
-    filteredMusicListElem: []
-    musicListElems = parsedHtml.select("#music_list")[0].find_all("div")
+    musicListElems = parsedHtml.select("#music_list")[0].find_all("div", "list_data")
+    for musicListElem in musicListElems:
+        tds = parsedHtml.find("table").find_all("td");
+        musicName = tds[1].text;
+        artistName = tds[3].text;
+
+        lis = tds[4].find_all("li");
+        basicLevel = int(float(lis[1].text) * 10.0);
+        advancedLevel = int(float(lis[3].text) * 10.0);
+        extremeLevel = int(float(lis[5].text) * 10.0);
+        print(f"{musicName}, {artistName}, {basicLevel}, {advancedLevel}, {extremeLevel}")
+
 
 def processLicenseMusic():
     licenseMusicUrl = f"https://p.eagate.573.jp/game/jubeat/festo/information/music_list1.html?page="
@@ -13,7 +23,7 @@ def processLicenseMusic():
     # First of all, we have to query the page count.
     response = urllib.request.urlopen(f"{licenseMusicUrl}1")
     html = response.read();
-    parsedHtml = BeautifulSoup(html)
+    parsedHtml = BeautifulSoup(html, "html.parser")
     maxPageIndex = len(parsedHtml.select("#contents > div.page_navi > div.page")[0].find_all("div"))
     processMusicListCell(parsedHtml)
 
