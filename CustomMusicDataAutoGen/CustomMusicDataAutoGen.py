5075d36390ff7beb770c0d7800fbbf72df0e1903
jhcha <jhcha@bluehole.net>
2019-08-02 17:46:04 +0900

    - cmd json 자동 생성 거의 완료 - utf8로 file write가 안되서 오류나서 고쳐야함

--- CustomMusicDataAutoGen/CustomMusicDataAutoGen.py
+++ CustomMusicDataAutoGen/CustomMusicDataAutoGen.py
@@ -1,24 +1,34 @@
 import urllib.request
 import json
 import pykakasi
+import os
 from bs4 import BeautifulSoup
 
+ENABLE_LIVE_MODE = False
+MODE_NAME = ENABLE_LIVE_MODE and "live" or "dev"
 FESTO_MUSIC_VERSION = 10
 
 class MusicData:
-    musicId: int
-    musicName: str
-    romajiMusicName: str
-    artistName: str
-    romajiArtistName: str
-    basicLevel: int
-    advancedLevel: int
-    extremeLevel: int
-    musicVersion: int
+    music_id: int
+    music_name: str
+    romaji_music_name: str
+    artist_name: str
+    romaji_artist_name: str
+    basic_level: int
+    advanced_level: int
+    extreme_level: int
+    music_version: int
 
-    def __init__(self, musicId: int, musicVersion: int):
-        self.musicId = musicId
-        self.musicVersion = musicVersion
+    def __init__(self, music_id: int, music_version: int):
+        self.music_id = music_id
+        self.music_name = ""
+        self.romaji_music_name = ""
+        self.artist_name = ""
+        self.romaji_artist_name = ""
+        self.basic_level = 0
+        self.advanced_level = 0
+        self.extreme_level = 0
+        self.music_version = music_version
 
 kakasi = pykakasi.kakasi()
 kakasi.setMode("H","a")
@@ -29,86 +39,122 @@ kakasi.setMode("s", False)
 kakasi.setMode("C", True)
 japaneseConverter = kakasi.getConverter()
 
-musicDataPool = {int: MusicData}
-cmdJson: str = "{\"music\":{"
+music_data_pool: {int, MusicData} = {}
+cmd_json: str = "{\"music\":{"
 
 # Parse all of the music version from cmd json.
-def processMusicVersion():
-    cmdUrl = "https://raw.githubusercontent.com/ggomdyu/jubiinfo/master/jubiinfo.client/Resource/DataTable/customMusicDatas_dev.json"
+def process_music_version():
+    cmd_url = "https://raw.githubusercontent.com/ggomdyu/jubiinfo/master/jubiinfo.client/Resource/" \
+              "DataTable/customMusicDatas_dev.json"
 
-    request = urllib.request.Request(cmdUrl)
+    request = urllib.request.Request(cmd_url)
     response = urllib.request.urlopen(request)
-    cmdJson = response.read();
+    prev_cmd_json = response.read();
 
-    jsonData = json.loads(cmdJson)
-    for key, value in jsonData["music"].items():
-        musicId = int(key)
-        musicVersion = int(value[5])
+    json_data = json.loads(prev_cmd_json)
+    for key, value in json_data["music"].items():
+        music_id = int(key)
+        music_version = int(value[5])
 
-        musicDataPool[musicId] = MusicData(musicId, musicVersion)
+        music_data_pool[music_id] = MusicData(music_id, music_version)
 
 # Parse all of the original music data.
-def processMusicListCell(parsedHtml):
-    musicListElems = parsedHtml.select("#music_list")[0].find_all("div", "list_data")
+def process_music_list_cell(parsed_html):
+    musicListElems = parsed_html.select("#music_list")[0].find_all("div", "list_data")
     for musicListElem in musicListElems:
         tds = musicListElem.find("table").find_all("td")
 
-        musicCoverImagePath = tds[0].find("img").attrs['src']
-        musicIdStartPos = musicCoverImagePath.rfind("id") + 2
-        musicIdEndPos = musicCoverImagePath.rfind(".")
-        musicId = int(musicCoverImagePath[musicIdStartPos:musicIdEndPos])
+        music_cover_image_path = tds[0].find("img").attrs['src']
+        music_id_start_pos = music_cover_image_path.rfind("id") + 2
+        music_id_end_pos = music_cover_image_path.rfind(".")
+        music_id = int(music_cover_image_path[music_id_start_pos:music_id_end_pos])
 
-        musicName = tds[1].text
-        artistName = tds[3].text
+        music_name = tds[1].text
+        artist_name = tds[3].text
 
         lis = tds[4].find_all("li")
-        basicLevel = int(float(lis[1].text) * 10.0)
-        advancedLevel = int(float(lis[3].text) * 10.0)
-        extremeLevel = int(float(lis[5].text) * 10.0)
+        basic_level = int(float(lis[1].text) * 10.0)
+        advanced_level = int(float(lis[3].text) * 10.0)
+        extreme_level = int(float(lis[5].text) * 10.0)
 
-        if musicId in musicDataPool == False:
-            musicDataPool[musicId] = MusicData(musicId, FESTO_MUSIC_VERSION)
+        if music_id not in music_data_pool:
+            music_data_pool[music_id] = MusicData(music_id, FESTO_MUSIC_VERSION)
 
-        musicData = musicDataPool[musicId]
-        musicData.musicName = musicName
-        musicData.romajiMusicName = japaneseConverter.do(musicName)
-        musicData.artistName = artistName
-        musicData.romajiArtistName = japaneseConverter.do(artistName)
-        musicData.basicLevel = basicLevel
-        musicData.advancedLevel = advancedLevel
-        musicData.extremeLevel = extremeLevel
+        music_data = music_data_pool[music_id]
+        music_data.music_name = music_name
+        music_data.romaji_music_name = japaneseConverter.do(music_name).replace(" ", "")
+        music_data.artist_name = artist_name
+        music_data.romaji_artist_name = japaneseConverter.do(artist_name).replace(" ", "")
+        music_data.basic_level = basic_level
+        music_data.advanced_level = advanced_level
+        music_data.extreme_level = extreme_level
 
 # Parse all of the license music data.
-def processLicenseMusic():
-    licenseMusicUrl = f"https://p.eagate.573.jp/game/jubeat/festo/information/music_list1.html?page="
+def process_license_music():
+    license_music_url = f"https://p.eagate.573.jp/game/jubeat/festo/information/music_list1.html?page="
 
     # First of all, we have to query the page count.
-    request = urllib.request.Request(f"{licenseMusicUrl}1")
+    request = urllib.request.Request(f"{license_music_url}1")
     response = urllib.request.urlopen(request)
-    parsedHtml = BeautifulSoup(response.read().decode("Shift_JIS").encode("utf-8"), "html.parser", from_encoding='utf-8')
+    parsed_html = BeautifulSoup(response.read().decode("Shift_JIS").encode("utf-8"), "html.parser", from_encoding='utf-8')
 
-    maxPageIndex = len(parsedHtml.select("#contents > div.page_navi > div.page")[0].find_all("div"))
-    processMusicListCell(parsedHtml)
+    max_page_index = len(parsed_html.select("#contents > div.page_navi > div.page")[0].find_all("div"))
 
-    for i in range(2, maxPageIndex):
-        break
+    print(f"License music processing... ({1}/{max_page_index})")
+    process_music_list_cell(parsed_html)
 
-def processOriginalMusic():
-    originalMusicUrl = f"https://p.eagate.573.jp/game/jubeat/festo/information/music_list2.html?page="
+    # for i in range(2, max_page_index + 1):
+    #     time.sleep(0.5)
+    #
+    #     request2 = urllib.request.Request(f"{license_music_url}{i}")
+    #     response2 = urllib.request.urlopen(request2)
+    #     parsed_html2 = BeautifulSoup(response2.read(), "html.parser", from_encoding='utf-8')
+    #
+    #     print(f"License music processing... ({i}/{max_page_index})")
+    #     process_music_list_cell(parsed_html2)
+
+def process_original_music():
+    original_music_url = f"https://p.eagate.573.jp/game/jubeat/festo/information/music_list2.html?page="
 
     # First of all, we have to query the page count.
-    request = urllib.request.Request(f"{originalMusicUrl}1")
+    request = urllib.request.Request(f"{original_music_url}1")
     response = urllib.request.urlopen(request)
-    parsedHtml = BeautifulSoup(response.read().decode("Shift_JIS").encode("utf-8"), "html.parser",
+    parsed_html = BeautifulSoup(response.read().decode("Shift_JIS").encode("utf-8"), "html.parser",
                                from_encoding='utf-8')
 
-    maxPageIndex = len(parsedHtml.select("#contents > div.page_navi > div.page")[0].find_all("div"))
-    processMusicListCell(parsedHtml)
+    max_page_index = len(parsed_html.select("#contents > div.page_navi > div.page")[0].find_all("div"))
 
-    for i in range(2, maxPageIndex):
-        break
+    print(f"Original music processing... ({1}/{max_page_index})")
+    process_music_list_cell(parsed_html)
+
+    # for i in range(2, max_page_index + 1):
+    #     time.sleep(0.5)
+    #
+    #     request2 = urllib.request.Request(f"{original_music_url}{i})")
+    #     response2 = urllib.request.urlopen(request2)
+    #     parsed_html2 = BeautifulSoup(response2.read().decode("Shift_JIS").encode("utf-8"), "html.parser", from_encoding='utf-8')
+    #
+    #     print(f"Original music processing... ({i}/{max_page_index})")
+    #     process_music_list_cell(parsed_html2)
 
 if __name__ == "__main__":
-    processMusicVersion()
-    processLicenseMusic()
-    processOriginalMusic()
+    process_music_version()
+    process_license_music()
+    process_original_music()
+
+    for music_data in music_data_pool.values():
+        print(music_data.basic_level)
+        cmd_json += f"\"{music_data.music_id}\":[\"{music_data.artist_name}\",\"{music_data.romaji_artist_name}\",{music_data.basic_level}," \
+                    f"{music_data.advanced_level},{music_data.basic_level},{music_data.music_version}],"
+    cmd_json = cmd_json[:-1]
+    cmd_json += "}}"
+
+    print(cmd_json)
+
+desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+cmd_json_path = desktop_path + "/customMusicDatas_dev.json"
+
+file = open(cmd_json_path, 'w');
+file.write(cmd_json)
+
+print("CMD json write complete!")
